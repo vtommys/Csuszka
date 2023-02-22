2d5e155f6b4aceb693296681ebbffb100004e435
vtommys <vtommys@gmail.com>
2023-02-22 18:48:59 +0100

    Left move

---
+++ script/script.js
@@ -0,0 +1,42 @@
+console.log("Start")
+document.body.style.height=window.innerHeight+'px';
+
+let nagysag=3;
+document.getElementById('palya').style.width=nagysag*200+'px';
+
+let tomb=[];
+for(let i=1;i<=nagysag;i++){
+    let sor=document.createElement('div');
+    sor.className='sor';
+    for(let j=1;j<=nagysag;j++){
+        let cella=document.createElement('div');
+        cella.className='cella';
+
+        do{
+            var szam=Math.round(Math.random()*(nagysag*nagysag-1));
+            for (var num of tomb){
+                if(num===szam) break;
+            }
+        }while(num===szam);
+
+        tomb.push(szam);
+        if(szam==0){
+            cella.classList.add('ures');
+        } else{
+            cella.innerHTML=szam;
+        }
+        cella.onclick=csuszik;
+        sor.appendChild(cella);
+    }
+    document.getElementById('palya').appendChild(sor);
+}
+
+function csuszik(){
+    console.log("Csúszik", this);
+    let mozog=false;
+    if((this.previousElementSibling) && (this.previousElementSibling.classList[1]=="ures")){
+        this.classList.add('bal');
+        mozog=true;
+    }
+    if(mozog) setTimeout(function(){},1000)
+};
